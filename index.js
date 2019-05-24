fbab31ee461e63de15ac0111089d351831156a5a
Miné E <mine_esen@yahoo.ca>
2019-05-24 21:12:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function takeANumber(katzDeliLine, name){
   for (let i= 0; i < katzDeliLine.length; i++){
-    return (`Welcome, ${name}. You are number ${i} in line.`);
+    
   }
+  return (`Welcome, ${name}. You are number ${i} in line.`);
 }
