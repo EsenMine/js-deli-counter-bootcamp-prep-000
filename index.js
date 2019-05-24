91632c2b0114627eda7d54262b5bff20e8ccb00b
Miné E <mine_esen@yahoo.ca>
2019-05-24 20:59:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name){
+  for (katzDeliLine = 0; katzDeliLine<100; katzDeliLine++){
+    console.log(`Welcome, ${name}. You are number ${katzDeliLine} in line.`);
+    break;
+  }
+}
